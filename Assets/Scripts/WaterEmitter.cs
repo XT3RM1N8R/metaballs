8f4f035ecad9fb9463af7476931509055bd65a8d
Darien Sokolov <darien.sokolov@yahoo.com>
2018-06-28 00:38:43 -0500

    idk what this junk is

---
+++ Assets/Scripts/WaterEmitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEmitter : MonoBehaviour {
+  public bool canEmit = true;
+  public int countDown = 1000;
+  public int emitVelocity = 100;
+  public Rigidbody sourceObject;
+  public int currentCount;
+	// Use this for initialization
+	void Start () {
+    currentCount = countDown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentCount <= 0 && canEmit) {
+      Rigidbody waterSphere = (Rigidbody)Instantiate(sourceObject, /*transform.position, transform.rotation,*/ transform);
+      waterSphere.velocity = new Vector3(0, emitVelocity * -1, 0);
+      currentCount = countDown;
+    } else {
+      if (currentCount > 0) {
+        currentCount--;
+      }
+    }
+	}
+}
